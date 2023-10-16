2835da65350bbac3c46e783cb70f2697f173afe3
steve <333799267@qq.com>
2023-10-16 20:44:55 +0800

    laster

--- dwm.c
+++ dwm.c
@@ -944,6 +944,7 @@ void drawbar(Monitor *m) {
   /* draw status first so it can be overdrawn by tags later */
   if (m == selmon) { /* status is only drawn on selected monitor */
     drawstatusbar(m, bh, stext);
+    updatesystray();
   }
 
   resizebarwin(m);
