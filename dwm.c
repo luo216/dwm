ad9f1085358c89619969459c099433054e991e42
steve <3337990267@qq.com>
2023-12-02 11:31:28 +0800

    Place the new winddown client at the end of the stack

--- dwm.c
+++ dwm.c
@@ -250,6 +250,7 @@ static int applysizehints(Client *c, int *x, int *y, int *w, int *h,
 static void arrange(Monitor *m);
 static void arrangemon(Monitor *m);
 static void attach(Client *c);
+static void attachbottom(Client *c);
 static void attachstack(Client *c);
 static void buttonpress(XEvent *e);
 static void click_notify(const Arg *arg);
@@ -424,6 +425,14 @@ struct NumTags {
 };
 
 /* function implementations */
+void attachbottom(Client *c) {
+  Client **tc;
+  c->next = NULL;
+  for (tc = &c->mon->clients; *tc; tc = &(*tc)->next)
+    ;
+  *tc = c;
+}
+
 void applyrules(Client *c) {
   const char *class, *instance;
   unsigned int i;
@@ -1267,7 +1276,7 @@ void manage(Window w, XWindowAttributes *wa) {
     c->isfloating = c->oldstate = trans != None || c->isfixed;
   if (c->isfloating)
     XRaiseWindow(dpy, c->win);
-  attach(c);
+  attachbottom(c);
   attachstack(c);
   XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32,
                   PropModeAppend, (unsigned char *)&(c->win), 1);
@@ -1716,7 +1725,7 @@ void sendmon(Client *c, Monitor *m) {
   detachstack(c);
   c->mon = m;
   c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
-  attach(c);
+  attachbottom(c);
   attachstack(c);
   focus(NULL);
   arrange(NULL);
@@ -2196,7 +2205,7 @@ int updategeom(void) {
         m->clients = c->next;
         detachstack(c);
         c->mon = mons;
-        attach(c);
+        attachbottom(c);
         attachstack(c);
       }
       if (m == selmon)
