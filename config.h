ee95615c113c467ac6d9a36a76cb4888ef4b3ab2
steve <3337990267@qq.com>
2024-05-29 12:48:07 +0800

    pvz is floating

--- config.h
+++ config.h
@@ -49,9 +49,10 @@ static const Rule rules[] = {
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING) = title
      */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    {"Gimp",      NULL,       NULL,       0,            1,           -1},
-    {"Firefox",   NULL,       NULL,       1 << 8,       0,           -1},
+    /* class        instance    title       tags mask     isfloating   monitor */
+    {"Gimp",        NULL,       NULL,       0,            1,           -1},
+    {"explorer.exe",NULL,       NULL,       0,            1,           -1},
+    {"Firefox",     NULL,       NULL,       1 << 8,       0,           -1},
 };
 
 /* layout(s) */
