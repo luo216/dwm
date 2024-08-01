c8aee11e51b35b5b4fcc43c325e503b34eb72487
HJ-Zhang <hjzhang216@gmail.com>
2024-08-01 15:03:32 +0800

    systray set classhint dwmsystray

--- dwm.c
+++ dwm.c
@@ -675,6 +675,8 @@ clientmessage(XEvent *e)
 			XAddToSaveSet(dpy, c->win);
 			XSelectInput(dpy, c->win, StructureNotifyMask | PropertyChangeMask | ResizeRedirectMask);
 			XReparentWindow(dpy, c->win, systray->win, 0, 0);
+      XClassHint ch = {"dwmsystray", "dwmsystray"};
+      XSetClassHint(dpy, c->win, &ch);
 			/* use parents background color */
 			swa.background_pixel  = scheme[SchemeNorm][ColBg].pixel;
 			XChangeWindowAttributes(dpy, c->win, CWBackPixel, &swa);
